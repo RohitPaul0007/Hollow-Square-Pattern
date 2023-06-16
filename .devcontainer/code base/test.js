76bf5b7e3e2d5902d6cc926a598a2aca834b55c5
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-06-16 07:35:44 +0000

    Config File Done

---
+++ .devcontainer/code base/test.js
@@ -0,0 +1,27 @@
+//# Hollow Square Pattern 
+function printPartialRow(n) {
+    let str= "";
+    str=str + "*";
+    for (let i=0;i<n-2;i++){
+      str = str + " "
+    }
+    str= str + "*";
+    alert(str);
+    }
+  function printSingleFullRow(n){
+  let str = "";
+  for(let i=0;i<n;i++){
+     str = str + "*";
+   }
+  alert(str);
+  }
+  function printWholething(n){
+    printSingleFullRow(n);
+    for ( let i=0;i<n-2;i++){
+      printPartialRow(n)
+    }
+    printSingleFullRow(n);
+  }
+  printWholething(10)
+  
+  
